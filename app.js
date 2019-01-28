7994263b2a2c2a1b6caa7d21731562e7cc13ff92
Ivan Rodrigues <ivan.silva@cvccorp.com.br>
2019-01-28 21:57:57 -0200

    included for a while the obligation to put HEADER only in GET of users

--- app.js
+++ app.js
@@ -40,16 +40,21 @@ app.use((err, req, res, next) =>{
 });
 
 function verifyJWT(req, res, next){
-  var token = req.headers['x-access-token'];
-  if (!token) return res.status(401).send({ auth: false, message: 'No token provided.' });
-  
-  jwt.verify(token, process.env.SECRET, function(err, decoded) {
-    if (err) return res.status(500).send({ auth: false, message: 'Failed to authenticate token.' });
+
+  //Temporário, depois vai ter que ter um controle mais fino...
+  if(req.method == 'GET') {
+    var token = req.headers['x-access-token'];
+    if (!token) return res.status(401).send({ auth: false, message: 'No token provided.' });
     
-    // se tudo estiver ok, salva no request para uso posterior
-    req.userId = decoded.id;
-    next();
-  });
+    jwt.verify(token, process.env.SECRET, function(err, decoded) {
+      if (err) return res.status(500).send({ auth: false, message: 'Failed to authenticate token.' });
+      
+      // se tudo estiver ok, salva no request para uso posterior
+      req.userId = decoded.id;
+      //next();
+    });
+  }
+  next();
 }
 
 module.exports = app;
