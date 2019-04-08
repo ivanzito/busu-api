90e339b15721424afa988e2357a9a31fd5e756b4
Ivan Rodrigues <ivanzito.jr@gmail.com>
2019-04-07 23:03:45 -0300

    refatorado o uso de Promises

--- routes/users.js
+++ routes/users.js
@@ -2,58 +2,16 @@ const express = require('express')
 const router = express.Router()
 const userService = require('../service/userService')
 const loginService = require('../service/loginService')
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  /*const db = require('../config/db/mongoose')
-  const Users = db.Mongoose.model('users', db.UserSchema, 'users')
-  Users.find()
-       .lean()
-       .exec((e,docs) => {res.json(docs).end()})*/
-
-
-  res.json(userService.findAll()).end()
-  
+  return Promise.resolve(userService.findAll())
+                .then(onfulfilled => {res.send(onfulfilled).end()})
 })
 
 /* POST users login */
-router.post('/login', function(req, res, next) {
-
-  /*
-  const db = require('../config/db/mongoose')
-  const Users = db.Mongoose.model('users', db.UserSchema, 'users')
-  Users.findOne({'email': req.body.email})
-       .exec((err, user) => {
-        if(err) {
-          res.status(500).json(err).end()
-        }
-
-        if(user == null) {
-          res.status(401).json("Usuario nao encontrado").end()
-        }
-
-        let response = res
-        bcrypt.compare(req.body.password  , user.password, (err, res) => {
-          if(res) {
-            // expires in 5min
-            let token = jwt.sign({ 'user': user },  process.env.SECRET, {expiresIn: 3000});
-            
-            console.log({'auth': true, 'token': token})
-            response.status(200).send({'auth': true, 'token': token}).end()
-          } else {
-            response.status(401).json(user).end()
-          } 
-        })        
-  }) */
-
-
-  let user = userService.findUserByEmail(req.body.email)
-  
-  console.log(user)
-
-  let response = loginService.login(user.email, req.body.password)
-  
- 
-
+router.post('/login', function(req, res, next) {  
+  let response = loginService.login(req.body.mail, req.body.password)
   if(response.auth) return res.status(200).send(response).end()
   else res.status(401).send(response).end()
 
@@ -62,28 +20,13 @@ router.post('/login', function(req, res, next) {
 
 /* POST users listing. */
 router.post('/', function(req, res, next) {
- /*
-  const db = require('../config/db/mongoose')
-  let Users = db.Mongoose.model('users', db.UserSchema, 'users')
-  bcrypt.genSalt(saltRounds, (err, salt) => {
-    bcrypt.hash(req.body.password, salt, (err, hash) => {
-      let newUser = new Users({ 'email': req.body.email, 'password': hash })
-      newUser.save((err) => {
-        if (err) {
-            res.status(500)
-              .json({ error: err.message })
-              .end()
-            return
-        }
-        res.json(newUser).end()
-      })
-    })
-  })
- */
-
- let user = userService.newUser(req.body.email, req.body.password)
- if(user instanceof Users) return user
- else "Não foi possível criar o usuário"
+ let user = userService.newUser(req.body.mail, req.body.password)
+  if(user) res.json(user)
+             .status(200)
+             .end()
+ else res.json("{error:'Não foi possível criar o usuário'}")
+         .status(500)
+         .end()
 })
 
 module.exports = router
--- service/loginService.js
+++ service/loginService.js
@@ -1,19 +1,18 @@
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
+const userService = require('../service/userService')
 
-
-  const login = (pwdUser, pwdRequestBody) => function() {
-    bcrypt.compare(pwdUser  , pwdRequestBody, (err, res) => {
+module.exports = {
+  login: function(email, pwdRequestBody) {
+    let user = userService.findByEmail(email)
+    bcrypt.compare(user.password, pwdRequestBody, (err, res) => {
       if(res && !err) {
         // expires in 5min
         let token = jwt.sign({ 'user': user },  process.env.SECRET, {expiresIn: 3000});
-        
         return `{'auth':true, 'token':${token}}`
       } else {
         return "{auth: false, 'token':''}"
       } 
     }) 
   }
-
-
-module.exports = {login}
+}
